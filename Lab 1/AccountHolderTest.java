0bacff80f5cdcae84dea473171001288684e18dc
Antonio Jackson, SR <ajackson7@hawk.iit.edu>
2020-01-25 18:02:26 -0600

    added data accessors

--- Lab 1/AccountHolderTest.java
+++ Lab 1/AccountHolderTest.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class AccountHolderTest {
 
 	public static void main(String[] args) {
@@ -7,9 +9,21 @@ public class AccountHolderTest {
         Scanner sc= new Scanner(System.in);
         double balance = 0;
        
-        //start an object
-        AccountHolder ah1 = new AccountHolder(1000.00); 
+        //start an object and access data for static fields
+        
+        AccountHolder.annualInterestRate = .04;
+        AccountHolder ah1 = new AccountHolder(); 
+        
+        //Get balance from user
+        balance = JOptionPane.showInputDialog("What is your initial balance?)
         ah1.setBalance(balance);
+        
+        //I could use a switch statement for the withdrawal and deposit info
+        //or I could list them seperately while calling the objects from the
+        //AHClass
+        
+        
+        
         System.out.println("Balance = " + ah1.getBalance());
         
 	}
